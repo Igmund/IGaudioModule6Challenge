0698e517c063a745ed68b80e54fe321e50dd8844
Ignazio Gaudio <i_gaudio@hotmail.com>
2023-05-10 00:20:37 +1000

    forecast data added to javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 const apiKey = "43b7357822a36d0b892e1f9c4cb1bc5e";
-var city = 
+//var city = Need Geocoding API to implement conversion from long/lat
+//Use same text $$$ ways
 
 //Get current weather in "city" - potential to use async?
 function getCurrentWeather(city) {
@@ -17,6 +18,7 @@ function getCurrentWeather(city) {
     return data;
   }
   
+  //pull API info and assign const to the specific data
   function displayCurrentWeather(data) {
     const city = data.name;
     const date = new Date(data.dt * 1000).toLocaleDateString();
@@ -25,6 +27,7 @@ function getCurrentWeather(city) {
     const humidity = data.main.humidity;
     const windSpeed = data.wind.speed;
     
+    //Add info into html page to display
   const weatherContainer = document.getElementById('weather-container');
   weatherContainer.innerHTML = `
     <h2>${city}</h2>
@@ -35,3 +38,55 @@ function getCurrentWeather(city) {
     <p>Wind Speed: ${windSpeed} m/s</p>
   `;
 }
+
+//Forecast display
+function displayForecast(data) {
+    //Iterate 5 days over - check slice vs i++
+    const forecastItems = data.list.slice(0, 5);
+    const forecastContainer = document.getElementById('forecast-container');
+    forecastContainer.innerHTML = '';
+  
+ // Loop through each of the required items and display
+ forecastItems.forEach((item) => {
+    const date = new Date(item.dt * 1000).toLocaleDateString();
+    const icon = item.weather[0].icon;
+    const minTemperature = item.main.temp_min;
+    const maxTemperature = item.main.temp_max;
+    //Add elements - check criteria
+
+    // New element for forecast
+    const card = document.createElement('div');
+    card.classList.add('forecast-card');
+    card.innerHTML = `
+      <h3>${date}</h3>
+      <img src="http://openweathermap.org/img/w/${icon}.png" alt="Weather Icon">
+      <p>Min Temperature: ${minTemperature}°C</p>
+      <p>Max Temperature: ${maxTemperature}°C</p>
+    `;
+
+    // Append the card to the forecast container
+    forecastContainer.appendChild(card);
+  });
+}
+
+  // Handlesearch - need to convert to co-ords via other API
+// async function handleSearch() {
+//     const cityInput = document.getElementById('city-input');
+//     const city = cityInput.value.trim();
+  
+//     if (city !== '') {
+//       try {
+//         // Get current weather data
+//         const currentWeather = await getCurrentWeather(city);
+//         displayCurrentWeather(currentWeather);
+  
+//         // Get forecast data
+//         const forecast = await getForecast(city);
+//         displayForecast(forecast);
+//       } catch (error) {
+//         console.log('An error occurred:', error)
+
+//       }
+//     }
+// };
+
