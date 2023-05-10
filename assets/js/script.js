4da8356492056ce1d54569b6765526fb452eb0e4
Ignazio Gaudio <i_gaudio@hotmail.com>
2023-05-11 00:18:18 +1000

    added framework to start saving city history, needs to be re-arranged into new function

--- assets/js/script.js
+++ assets/js/script.js
@@ -83,3 +83,54 @@ form.addEventListener("submit", function(event) {
       console.log("Error:", error);
     });
 });
+/////
+// Get the city-history div element
+const cityHistoryDiv = document.getElementById('city-history');
+
+// Initialize an empty array to store the last 8 cities
+let cityHistory = [];
+
+// Load the city history from localStorage if available
+if (localStorage.getItem('cityHistory')) {
+  cityHistory = JSON.parse(localStorage.getItem('cityHistory'));
+
+  // Add each city to the city-history div as a button
+  for (let i = 0; i < cityHistory.length; i++) {
+    const cityButton = document.createElement('button');
+    cityButton.innerText = cityHistory[i];
+    cityButton.addEventListener('click', () => {
+      // Refetch the weather data for the selected city and display the results
+      fetchWeather(cityHistory[i]);
+    });
+    cityHistoryDiv.appendChild(cityButton);
+  }
+}
+
+// Store the city name in the cityHistory array
+cityHistory.unshift(cityName);
+
+// Remove the oldest city if the cityHistory array has more than 8 items
+if (cityHistory.length > 8) {
+  cityHistory.pop();
+}
+
+        // Save the cityHistory array to localStorage
+        localStorage.setItem('cityHistory', JSON.stringify(cityHistory));
+
+        // Create a new city button and add it to the city-history div
+        const cityButton = document.createElement('button');
+        cityButton.innerText = cityName;
+        cityButton.addEventListener('click', () => {
+          // Refetch the weather data for the selected city and display the results
+          fetchWeather(cityName);
+        });
+        cityHistoryDiv.insertBefore(cityButton, cityHistoryDiv.firstChild);
+
+        // Refetch the weather data for the selected city and display the results
+        fetchWeather(cityName);
+      } else {
+        console.error('City not found');
+      }
+
+    .catch(error => console.error(error));
+
